f89121ad1ae8b2dcb720a8ad9deab7a5025009c4
HMLEE <hmlee@wylie.co.kr>
2026-03-11 10:54:33 +0900

    경로 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,10 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding"
   ],
-  "framework": "@storybook/react-vite"
+  "framework": "@storybook/react-vite",
+  "viteFinal": async (config) => {
+    config.base = '/imbank_storybook/';
+    return config;
+  },
 };
 export default config;
