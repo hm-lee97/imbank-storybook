65b83ba1135c4c24cad894087dfc342dfcac7029
hm-lee97 <hmlee@wylie.co.kr>
2026-03-11 01:49:31 +0000

    Deploying to gh-pages from @ hm-lee97/imbank-storybook@64dd8163df66f27dd7a2f95a470cb3e277802fa1 🚀

---
+++ assets/Header.stories-KC8y2A4o.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-Ba6BM87J.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-CH6SwbKc.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-C-9jgQzJ.js";const{fn:r}=__STORYBOOK_MODULE_TEST__,p={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,d as __namedExportsOrder,p as default};
